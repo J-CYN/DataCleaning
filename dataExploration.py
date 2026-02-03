d89973cbc9c6cd5be0cac49a5e4833033f4a431c
Jamal Abudiab <jabudiab2024@gmail.com>
2026-02-03 19:41:16 +0000

    Creation of basic data Exploration file and included instructions

---
+++ dataExploration.py
@@ -0,0 +1,13 @@
+import pandas as pd  # For data manipulation and analysis
+import numpy as np  # For numerical operations
+import matplotlib.pyplot as plt  # For data visualization
+from sklearn.model_selection import train_test_split  # For splitting data
+from sklearn.preprocessing import MinMaxScaler, StandardScaler  # For scaling
+from io import StringIO  # For reading string data as file
+import requests  # For HTTP requests to download data
+
+# Step 1:
+
+# Step 2:
+
+# Step 3:
